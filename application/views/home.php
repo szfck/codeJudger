0bfdeb8829b7f8eb72105626958579325d154a2a
Kai Chen <kchen9530@gmail.com>
2019-03-05 17:26:31 -0500

    Add home page

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Code Judger</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
+  </head>
+  <body>
+    <div class="container">
+        <h1>Welcome to Code Judger</h1>
+    </div>
+  </body>
+</html>
