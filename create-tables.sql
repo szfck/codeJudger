f3662350e8f2db99cc0197d0265a6e00cb92b392
rj-1234 <rj1234@nyu.edu>
2019-03-08 14:35:35 -0500

    Adding the email field in the user table.

--- create-tables.sql
+++ create-tables.sql
@@ -2,11 +2,10 @@ DROP DATABASE IF EXISTS judger;
 CREATE DATABASE judger;
 USE judger;
 CREATE TABLE user(`id` int NOT NULL AUTO_INCREMENT,
+  `useremail` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
-INSERT INTO `user` (`username`, `password`) VALUES 
-    ('kai', '123456'),
-    ('foo', '123456'),
-    ('bar', '123456');
+INSERT INTO `user` (`useremail`, `username`, `password`) VALUES 
+    ('kai@codejudger.com', 'kai', '123456');
