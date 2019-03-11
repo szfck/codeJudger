be19d6a7aa8ff1d5a81df65e2cfd5a8e73f4fbac
Kai Chen <kchen9530@gmail.com>
2019-03-11 11:05:13 -0400

    Fix issue: background image not show

--- application/views/home.php
+++ application/views/home.php
@@ -5,20 +5,21 @@
     <title>Welcome to Code Judger</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
     <style>
-    p.normal {font-weight:normal;}
-    p.light {font-weight:lighter;}
-    p.thick {font-weight:bold;}
-    p.thicker {font-weight:900;}
+        p.normal {font-weight:normal;}
+        p.light {font-weight:lighter;}
+        p.thick {font-weight:bold;}
+        p.thicker {font-weight:900;}
+
+        body {
+            background-image:url(<?=base_url("/images/coding.jpeg"); ?>);
+            background-repeat:no-repeat;
+            background-size:100% 100%;
+            background-attachment:fixed;
+        }
     </style>
   </head>
   <body>
-    {
-      background-image:url('coding.jpeg');
-      background-repeat:no-repeat;
-      background-position:right top;
-      margin-right:200px;
-    }
-    <div class="container">
+    <div class="container" style="background:url(application/views/images/coding.jpeg) no-repeat top right">
         <h1>Welcome to Code Judger</h1>
         <p class="thicker"> A New Way to Learn </p>
         <p class="thick"> Code Judger is a platform to help you enhance your skills, expand your knowledge and prepare for technical interviews.</p> 
