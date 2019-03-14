4521f942b4f70e90da01ffe5708ab38bac370110
rj-1234 <rj1234@nyu.edu>
2019-03-14 16:15:45 -0400

    Added test cases for email_check method of User_model

--- application/tests/models/User_model_test.php
+++ application/tests/models/User_model_test.php
@@ -9,7 +9,7 @@ class User_model_test extends TestCase
         $this->obj = $this->CI->User_model;
     }
 
-    public function test_login_user()
+    public function test_login_user_pass()
     {
         $expected = ['1', 'hello@codejudger.com', 'kai', '123456'];
 
@@ -21,6 +21,44 @@ class User_model_test extends TestCase
             $this->assertEquals($expected[$i], $value);
             $i = $i + 1;
         }
+    }
+
+    public function test_login_user_fail()
+    {
+        $expected = false;
+
+        $testcases = array(array('kai', '123'), 
+                            array('kia', '123456'), 
+                            array('kia', '23456'));
+
+        for($x = 0; $x < count($testcases); $x++){
+            print($x);
+            $name = $testcases[$x][0];
+            $pass = $testcases[$x][1];
+
+            $actual = $this->obj->login_user($name,$pass);            
+            $this->assertEquals($expected, $actual);
+
+        }
+        
+    }
+
+    public function test_email_check_pass(){
+        $email = 'hello@codejudger.com';
+
+        $expected = false;
+        $actual = $this->obj->email_check($email);
+        $this->assertEquals($expected, $actual);
+
+    }
+
+    public function test_email_check_fail(){
+        $email = 'invalid_email';
+        
+        $expected = true;
+        $actual = $this->obj->email_check($email);
+        $this->assertEquals($expected, $actual);
+
     }
 
 }
