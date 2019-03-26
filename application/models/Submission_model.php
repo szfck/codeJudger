a5e21cab81865c94edcb39663fc587d91e2b48af
Kai Chen <kchen9530@gmail.com>
2019-03-25 22:21:09 -0400

    Add fake judge server [always return success]

--- application/models/Submission_model.php
+++ application/models/Submission_model.php
@@ -3,7 +3,7 @@ class Submission_model extends CI_model{
     
     public function create_submission($problem_name, $user_id, $code, $type) {
         $query = $this->db->query('SELECT * FROM submission');
-        $sid = $query->num_rows() + 1;
+        $subid = $query->num_rows() + 1;
         $submission = array(
             'time' => time(),
             'problem' => $problem_name,
@@ -13,12 +13,37 @@ class Submission_model extends CI_model{
         );
         $this->db->insert('submission', $submission);
 
-        $path = FCPATH."/submissions/".$sid.".".$type;
+        $path = FCPATH."/submissions/".$subid.".".$type;
         
         $this->load->helper('file');
         if (!write_file($path, $code)) {
             die('Unable to write the file');
         }
+
+        // Create map with request parameters
+        $params = array ('submission_id' => $subid);
+
+        // Build Http query using params
+        $query = http_build_query ($params);
+
+        // Create Http context details
+        $contextData = array (
+                    'method' => 'POST',
+                    'header' => "Connection: close\r\n".
+                                "Content-Length: ".strlen($query)."\r\n".
+                                "Content-Type: "."application/x-www-form-urlencoded",
+                    'content'=> $query );
+
+        // Create context resource for our request
+        $context = stream_context_create (array ( 'http' => $contextData ));
+
+        // Read page rendered as result of your POST request
+        $result =  file_get_contents (
+                    'http://judger-judge:3000/judge',  // page url
+                    false,
+                    $context);
+
+
     }
 
     function get_user_submission_list() {
