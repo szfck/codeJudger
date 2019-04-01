1e15464dd085ce20709ba2fc974b985f75bf246d
Ariel Wang <bubugab@163.com>
2019-03-31 22:21:09 -0400

    add navigation to article page

--- application/views/article.php
+++ application/views/article.php
@@ -25,3 +25,17 @@ This is article page.
     <small class="text-muted">by Ariel Wang</small>
   </a>
 </div>
+
+<nav aria-label="Page navigation">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
