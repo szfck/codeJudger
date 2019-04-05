a617da1ce2855b17a675d5cc419e552659f66067
rj-1234 <rj1234@nyu.edu>
2019-04-05 18:16:43 -0400

    Fixed the Judge_py.sh python judger

--- docker/judge/judge_py.sh
+++ docker/judge/judge_py.sh
@@ -7,28 +7,23 @@ echo $filename
 problems_path=/judge/problems
 submissions_path=/judge/submissions
 
-compile_file=cpp${filename}.out
-
-rm -f /tmp/$compile_file
-
-
 TIME_LIMIT=5
 TIMEOUT_CODE=124
-SUCCESS_CODE=0
-COMPILE_ERROR_CODE=2
 
 for input in $problems_path/$problem/secret/*.in; do
     num=$(basename "$input" | cut -d. -f1)
 
     echo run $num
-    timeout $TIME_LIMIT python $submissions_path/$submission < $input > /tmp/$num.out
-    if [ $? -eq $SUCCESS_CODE ]; then
-        exit 0
-    fi
+    echo begin testing
+    timeout $TIME_LIMIT python $submissions_path/$submission < $input 2> /tmp/err.txt 1> /tmp/$num.out
 
-    if [ $? -eq $COMPILE_ERROR_CODE ]; then
+    #cat /tmp/err.txt
+    if [ "$(cat /tmp/err.txt)" ]; then
         exit 2
     fi
+
+    echo end testing
+    echo exitcode = $?
 
     # time limit exceed
     if [ $? -eq $TIMEOUT_CODE ]; then
