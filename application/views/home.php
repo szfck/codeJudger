3a47011e9b273854f32e4f1075d4b034a04f6147
Kai Chen <kchen9530@gmail.com>
2019-04-09 23:12:13 -0400

    Format home page in center

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,23 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
 
-<div class="welcome-banner">
-    <h1>Welcome to Code Judger</h1>   
-</div>
 
-<div>
-    <p class="thicker">The Best Way to Learn</p>
+<div class="container">
+    <div class="welcome-banner">
+        <h1>Welcome to Code Judger</h1>
+    </div>
+    <div>
+        <p class="thicker">The Best Way to Learn</p>
 
-    <p class="thick"> Code Judger is a platform to improve your skills and prepare for interviews </p>
-</div>
+        <p class="thick"> Code Judger is a platform to improve your skills and prepare for interviews </p>
+    </div>
 
-<div class="problem-list">
-    <?php include 'problem_list.php'; ?>
+    <div class="problem-list">
+        <?php include 'problem_list.php'; ?>
+    </div>
+
 </div>
