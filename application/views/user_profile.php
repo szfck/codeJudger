6d243636ae6369e9bc5fa48c1a5bc26be8793aec
rj-1234 <rj1234@nyu.edu>
2019-04-14 13:43:03 -0400

    simplified the user_profile view

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -7,23 +7,11 @@
 <title><?=$_SESSION['user_name']?>'s Dashboard CodeJudger</title>
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-4">
-      <table class="table table-bordered table-striped">
-        <tr>
-            <th colspan="2"><h4 class="text-center">User Info</h3></th>
-        </tr>
-          <tr>
-            <td>User Name</td>
-            <td><?php echo $this->session->userdata('user_name'); ?></td>
-          </tr>
-      </table>
-    </div>
+  <div class="d-flex flex-row-reverse">
+    <div class="p-2">
+      <a href="<?php echo base_url('user/user_logout');?>">
+        <button type="button" class="btn-primary">Logout</button>
+      </a></div>
   </div>
-  <a href="<?php echo base_url('user/user_logout');?>" >  
-    <button type="button" class="btn-primary">Logout</button> 
-  </a> 
-
-    <?php $this->load->view('submission') ?>
-
+  <?php $this->load->view('submission') ?>
 </div>
