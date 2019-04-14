b8fe11d4e0b0e65cce2784b8429a0ba715eafc57
rj-1234 <rj1234@nyu.edu>
2019-04-14 16:47:34 -0400

    Improved problem list view

--- application/views/problem_list.php
+++ application/views/problem_list.php
@@ -1,26 +1,34 @@
 
-<table class="table table-striped">
-<thead>
-  <h1>Category <span class="badge badge-secondary"></span></h1>
- <tr>
-  <a href="#" class="badge badge-primary">Array</a>
-  <a href="#" class="badge badge-secondary">Sort</a>
-  <a href="#" class="badge badge-success">Tree</a>
-  <a href="#" class="badge badge-danger">Heap</a>
-  <a href="#" class="badge badge-warning">Stack</a>
-  <a href="#" class="badge badge-info">Queue</a>
-  <a href="#" class="badge badge-light">Graph</a>
-  <a href="#" class="badge badge-dark">Recursion</a>
- </tr>
-</thead>
-<tbody>         
-    <?php 
-        $this->load->helper('problem_helper');
-        $problem_number = 1;
-        foreach (get_problem_list() as $problem ) {
-            echo "<tr> <td>$problem_number."." ".anchor("problem/get_problem/".$problem."/".$problem_number, ucfirst($problem), array('class' => 'problems'))."</td> </tr>";
-            $problem_number++;
-        }
-    ?>
-</tbody>
+<table class="table">
+    <thead class="thead-light">
+    <!-- <h1>Category <span class="badge badge-secondary"></span></h1>
+    <tr>
+    <a href="#" class="badge badge-primary">Array</a>
+    <a href="#" class="badge badge-secondary">Sort</a>
+    <a href="#" class="badge badge-success">Tree</a>
+    <a href="#" class="badge badge-danger">Heap</a>
+    <a href="#" class="badge badge-warning">Stack</a>
+    <a href="#" class="badge badge-info">Queue</a>
+    <a href="#" class="badge badge-light">Graph</a>
+    <a href="#" class="badge badge-dark">Recursion</a>
+    </tr> -->
+
+        <tr>
+            <th scope="col" style="width:5%;">#</th>
+            <th scope="col">Problem</th>
+        </tr>
+    </thead>
+    <tbody>         
+        <?php 
+            $this->load->helper('problem_helper');
+            $problem_number = 1;
+            foreach (get_problem_list() as $problem ) {
+                echo "<tr>";
+                echo "<th scope='row'>".$problem_number."</th>";
+                echo "<td> ".anchor("problem/get_problem/".$problem."/".$problem_number, ucfirst($problem), array('class' => 'problems'))."</td>";
+                echo "</tr>";
+                $problem_number++;
+            }
+        ?>
+    </tbody>
 </table>
