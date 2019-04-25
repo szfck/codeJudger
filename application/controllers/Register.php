963867e3d454d5a7e7987ef689247dbacfc2ad4c
Kai Chen <kchen9530@gmail.com>
2019-04-25 17:27:08 -0400

    Add user existence check when register

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -21,16 +21,20 @@ class Register extends CI_Controller {
 
     public function register_user(){
         $user=array(
-        'useremail'=>$this->input->post('user_email'),
-        'username'=>$this->input->post('user_name'),
-        'password'=>md5($this->input->post('user_password')),
-        'role'=>'user',
+            'useremail'=>$this->input->post('user_email'),
+            'username'=>$this->input->post('user_name'),
+            'password'=>md5($this->input->post('user_password')),
+            'role'=>'user',
         );
         print_r($user);
    
         $email_check=$this->user_model->email_check($user['useremail']);
+        $is_user_exist = $this->user_model->get_user_details($user['username']);
    
-        if($email_check){
+        if ($is_user_exist) {
+            $this->session->set_flashdata('error_msg', 'User already exists,Try again.');
+            redirect('register');
+        } else if($email_check){
             $this->user_model->register_user($user);
             $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
             redirect('login');
